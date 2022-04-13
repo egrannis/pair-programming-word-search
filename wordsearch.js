09db6eada6d2e66c3bb30f58448e5fceba03e3d4
Emma Grannis <grannis.emma@gmail.com>
2022-04-13 20:04:05 +0000

    Pair programmed with @NateBainer

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+// using transposition function from previous
+const transpose = function(matrix) {
+  const result = Array.from({length: matrix[0].length}, () => new Array(matrix.length).fill());
+  
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[0].length; c++) {
+      result[c][r] = matrix[r][c];
     }
-}
+  }
+  return result;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let str of horizontalJoin) {
+    if (str.includes(word)) return true;
+  }
+  const transposed = transpose(horizontalJoin);
+  for (let stri of transposed) {
+    if (stri.includes(word)) return true;
+  }
+  return false;
+};
+
+
+
+module.exports = wordSearch;
+
